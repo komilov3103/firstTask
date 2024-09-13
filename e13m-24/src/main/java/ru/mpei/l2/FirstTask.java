4a847155028ecd1dfa43c3acac1ae876ac5096c7
komilov3103 <komilovkomil156@gmail.com>
2024-09-13 11:21:20 +0300

    Task is done

--- e13m-24/src/main/java/ru/mpei/l2/FirstTask.java
+++ e13m-24/src/main/java/ru/mpei/l2/FirstTask.java
@@ -2,16 +2,30 @@ package ru.mpei.l2;
 
 public class FirstTask {
     public static void main(String[] args) {
-        int[] array = {5, 1, 9, 8, 11, 3};
+        int[] array = {7, 6, 2, 5, 3};
 
         for (int i = 0; i < array.length; i++){
             for (int j = 0; j < array.length - 1- i; j++){
                 if(array[j] > array[j+1]){
                     int temp = array[j];
                     array[j] = array[j+1];
-
+                    array[j+1] = temp;
                 }
             }
         }
+
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+
+        double median = 0;
+        int middleElement = array.length / 2;
+        if (array.length % 2 == 0){
+            median = (array[middleElement] + array[middleElement - 1]) / 2.0;
+        } else {
+            median = array[middleElement];
+        }
+        System.out.println();
+        System.out.println(median);
     }
 }
--- e13m-24/src/main/java/ru/mpei/l2/Lecture2.java
+++ e13m-24/src/main/java/ru/mpei/l2/Lecture2.java
@@ -3,16 +3,16 @@ package ru.mpei.l2;
 public class Lecture2 {
     public static void main(String[] args) {
         int variable = 5;
-        for (; variable < 35;){
+        for (; variable < 35; ) {
             variable += 5;
         }
 
-        for (String var : args){
+        for (String var : args) {
             System.out.println(var);
         }
         int number = 2;
 
-        switch (number){
+        switch (number) {
             case 1:
                 System.out.println("This is good number!");
                 break;
@@ -23,9 +23,9 @@ public class Lecture2 {
         }
 
         double[] numbersDouble = new double[3];
-        int[] numberInt = {1,2,3,4};
+        int[] numberInt = {1, 2, 3, 4};
         String[] arrayStrings = new String[]{"Komil", "Dilovar", "some"};
-        for (String var : arrayStrings){
+        for (String var : arrayStrings) {
             System.out.println(var);
         }
 
@@ -36,36 +36,12 @@ public class Lecture2 {
         k++;
 
         for (int i = 0; i <= 10; i++) {
-            if (i % 2 == 0 && i == 2){
+            if (i % 2 == 0 && i == 2) {
                 continue;
             } else if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
 
-        System.out.println(addTwoNumbers(3, 6));
-
-        double[] array = arrayInitializer(6);
-
-        for (double var : array){
-            System.out.println(var);
-        }
-
-
-
     }
-
-    public static double addTwoNumbers(double a, double b){
-        return a + b;
-    }
-
-    public static double[] arrayInitializer(int quantity){
-        double[] array = new double[quantity];
-        for (int i = 0; i < quantity; i++){
-            array[i] = 1;
-        }
-        return array;
-    }
-
-    public static void printArray()
 }
